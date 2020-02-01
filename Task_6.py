a34187226f9219a3ae4bbcb3b46348a4c34ed235
yogesshraj <54570181+yogesshraj@users.noreply.github.com>
2020-02-01 17:57:38 +0530

    Create Task_6.py

---
+++ Task_6.py
@@ -0,0 +1,23 @@
+import requests,json,pprint,os
+from bs4 import BeautifulSoup
+from Task_1 import osm,details
+list1=osm()
+c=[]
+url_list1=[]
+for i in list1:
+    url_list1.append(i["url"][27:39].strip("/"))
+a=details(url_list1)
+
+for i in a:
+	c.append(i["language"][0])
+a={}
+for i in c:
+	if i not in a:
+		a[i]=None
+for i in a:
+	count=0
+	for j in c:
+		if i==j:
+			count+=1
+	a[i]=count
+pprint.pprint (a)
